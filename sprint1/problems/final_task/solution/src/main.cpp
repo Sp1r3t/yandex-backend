cf0b2905fc36a1569caf52efa9ed3b9a7238d151
Tim <tksusha1973@gmail.com>
2026-03-31 21:22:32 +0300

    added to final task

---
+++ sprint1/problems/final_task/solution/src/main.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <boost/asio.hpp>
+#include <boost/asio/signal_set.hpp>
+
+#include "http_server.h"
+#include "json_loader.h"
+#include "request_handler.h"
+
+using namespace std::literals;
+
+namespace net = boost::asio;
+
+namespace {
+
+template <typename Fn>
+void RunWorkers(unsigned n, const Fn& fn) {
+    n = std::max(1u, n);
+
+    std::vector<std::thread> workers;
+    workers.reserve(n - 1);
+
+    while (--n) {
+        workers.emplace_back(fn);
+    }
+
+    fn();
+
+    for (auto& t : workers) {
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+}
+
+}  // namespace
+
+int main(int argc, const char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: game_server <config-file>"sv << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    try {
+        const auto game = json_loader::LoadGame(argv[1]);
+
+        const unsigned num_threads = std::max(1u, std::thread::hardware_concurrency());
+        net::io_context ioc(num_threads);
+
+        net::signal_set signals(ioc, SIGINT, SIGTERM);
+        signals.async_wait([&ioc](const boost::system::error_code&, int) {
+            ioc.stop();
+        });
+
+        const auto address = net::ip::make_address("0.0.0.0");
+        constexpr unsigned short port = 8080;
+
+        http_handler::RequestHandler handler{game};
+        http_server::ServeHttp(ioc, {address, port},
+                               [&handler](auto&& req, auto&& send) {
+                                   handler(std::forward<decltype(req)>(req),
+                                           std::forward<decltype(send)>(send));
+                               });
+
+	std::cout << "Server has started..." << std::endl;
+        RunWorkers(num_threads, [&ioc] {
+            ioc.run();
+        });
+    } catch (const std::exception& ex) {
+        std::cerr << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
