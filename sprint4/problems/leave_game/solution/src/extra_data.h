3064c8afd669075fcbfb79de0e57ce8d02293643
Tim <tksusha1973@gmail.com>
2026-06-06 16:05:25 +0300

    Add leave_game solution: dog retirement with PostgreSQL
    Solution for sprint4 leave_game task: retired players stored in DB,
    GET /api/v1/game/records endpoint, GAME_DB_URL via env variable.

---
+++ sprint4/problems/leave_game/solution/src/extra_data.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <boost/json.hpp>
+#include <string>
+#include <unordered_map>
+
+namespace extra_data {
+
+namespace json = boost::json;
+
+class ExtraData {
+public:
+    void AddMapLootTypes(const std::string& map_id, json::array loot_types) {
+        loot_types_[map_id] = std::move(loot_types);
+    }
+
+    const json::array* GetMapLootTypes(const std::string& map_id) const {
+        auto it = loot_types_.find(map_id);
+        return it == loot_types_.end() ? nullptr : &it->second;
+    }
+
+private:
+    std::unordered_map<std::string, json::array> loot_types_;
+};
+
+}  // namespace extra_data
